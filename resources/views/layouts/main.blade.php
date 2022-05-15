e1055455dde42ad0820738f8700674d5bcc4936f
DjorroDev <aufarmawarid@gmail.com>
2022-05-15 11:49:59 +0700

    Add dynamic active nav link at different page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container">
-                <a class="navbar-brand" href="#"><span data-feather="briefcase"></span> Lokers</a>
+                <a class="navbar-brand" href="/"><span data-feather="briefcase"></span> Lokers</a>
                 <button class="navbar-toggler btn-light" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation">
@@ -25,10 +25,12 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link {{ Request::is('/') ? 'active' : '' }}" aria-current="page"
+                                href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/jobs">Job Search</a>
+                            <a class="nav-link {{ Request::is('jobs*') ? 'active' : '' }} " href="/jobs">Job
+                                Search</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Post Job</a>
