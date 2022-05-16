f91f5e023f6c3aa6240e9b80dc2ee48a89a8a487
DjorroDev <aufarmawarid@gmail.com>
2022-05-16 15:32:49 +0700

    Create backend side for register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
+use Illuminate\Auth\Events\Validated;
 
 class RegisterController extends Controller
 {
@@ -21,5 +21,42 @@ class RegisterController extends Controller
     public function recruiter()
     {
         return view('register.recruiter');
+    }
+
+    public function storeRecruiter(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email:dns|max:255',
+            'password' => 'required|min:5'
+        ]);
+
+        // Register as company or recruiter
+        $validatedData['is_recruiter'] = true;
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        user::create($validatedData);
+
+        return redirect('/login')->with('success', 'Account has been created.');
+    }
+
+    public function storeSeeker(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email:dns|max:255',
+            'password' => 'required|min:5'
+        ]);
+
+        // Register as job seeker
+        $validatedData['is_recruiter'] = false;
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+
+
+        user::create($validatedData);
+
+        return redirect('/login')->with('success', 'Account has been created.');
     }
 }
--- resources/views/register/seeker.blade.php
+++ resources/views/register/seeker.blade.php
@@ -3,19 +3,38 @@
 @section('container')
     <div class="row justify-content-center align-items-center my-5">
         <div class="col-md-5 p-5 shadow border">
-            <form class="">
+            <form action="/register/seeker" method="POST">
+                @csrf
                 <h1 class="h3 mb-5 fw-normal text-center">Please Register</h1>
                 <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="floatingInput" placeholder="your name">
+                    <input type="text" class="form-control @error('name') is-invalid @enderror" name="name"
+                        id="floatingInput" required placeholder="your name" value="{{ old('name') }}">
                     <label for="floatingInput">Full name</label>
+                    <div class="invalid-feedback">
+                        @error('name')
+                            {{ $message }}
+                        @enderror
+                    </div>
                 </div>
                 <div class="form-floating mb-3">
-                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                    <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                        id="floatingInput" required placeholder="name@example.com" value="{{ old('email') }}">
                     <label for="floatingInput">Email address</label>
+                    <div class="invalid-feedback">
+                        @error('email')
+                            {{ $message }}
+                        @enderror
+                    </div>
                 </div>
                 <div class="form-floating mb-2">
-                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                        id="floatingPassword" required placeholder="Password">
                     <label for="floatingPassword">Password</label>
+                    <div class="invalid-feedback">
+                        @error('password')
+                            {{ $message }}
+                        @enderror
+                    </div>
                 </div>
 
                 <button class="w-100 btn btn-lg btn-dark my-3" type="submit">register</button>
