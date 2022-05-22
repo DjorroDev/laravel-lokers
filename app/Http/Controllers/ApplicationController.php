756a4108ea41972be8da1272dd8d8c4d42df9a27
DjorroDev <aufarmawarid@gmail.com>
2022-05-22 22:27:30 +0700

    Create view and show

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Application;
 
 class ApplicationController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('dashboard.application.index', [
+            'apps' => Application::where('receiver_id', auth()->user()->id)->get()
+        ]);
+    }
+
+    public function show(Application $application)
+    {
+        return view('dashboard.application.show', [
+            'app' => $application
+        ]);
+    }
 }
