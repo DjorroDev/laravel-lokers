42b6ea9c836c9ce530bed9053d1f95cdf97e7f1b
DjorroDev <aufarmawarid@gmail.com>
2022-05-23 22:26:56 +0700

    Create custom middleware recruiter

--- routes/web.php
+++ routes/web.php
@@ -26,27 +26,27 @@ Route::get('/', function () {
 Route::get('/jobs', [ListingController::class, 'index']);
 Route::get('/jobs/{listing}', [ListingController::class, 'show']);
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/profile', [ProfileController::class, 'edit']);
-Route::put('/profile', [ProfileController::class, 'store']);
+Route::get('/profile', [ProfileController::class, 'edit'])->middleware('auth');
+Route::put('/profile', [ProfileController::class, 'update']);
 
 
-Route::get('/register', [RegisterController::class, 'index']);
-Route::get('/register/seeker', [RegisterController::class, 'seeker']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::get('/register/seeker', [RegisterController::class, 'seeker'])->middleware('guest');
 Route::post('/register/seeker', [RegisterController::class, 'storeSeeker']);
 
-Route::get('/register/company', [RegisterController::class, 'recruiter']);
+Route::get('/register/company', [RegisterController::class, 'recruiter'])->middleware('guest');
 Route::post('/register/company', [RegisterController::class, 'storeRecruiter']);
 
 Route::get('/dashboard', function () {
     return view('dashboard.index');
-});
+})->middleware('recruiter');
 
-Route::resource('/dashboard/lists', DashboardListingController::class);
+Route::resource('/dashboard/lists', DashboardListingController::class)->middleware('recruiter');
 
-Route::get('/dashboard/applications', [ApplicationController::class, 'index']);
-Route::get('/dashboard/applications/{application}', [ApplicationController::class, 'show']);
+Route::get('/dashboard/applications', [ApplicationController::class, 'index'])->middleware('recruiter');
+Route::get('/dashboard/applications/{application}', [ApplicationController::class, 'show'])->middleware('recruiter');
 Route::post('/jobs/apply/{applications}', [ApplicationController::class, 'store']);
