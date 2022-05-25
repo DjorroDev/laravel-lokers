5b61d4e6f53663c223c706e0066310d05cb9f54c
DjorroDev <aufarmawarid@gmail.com>
2022-05-25 17:51:34 +0700

    Update welcome page version 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,61 +1,55 @@
 @extends('layouts.main')
 
 @section('container')
-    <section>
-        <div class="card bg-dark text-white mb-5">
-            <img src="https://source.unsplash.com/1200x400/?seeker" style="opacity:30%"
-                class="card-img img-responsive d-sm-block" alt="jobs">
-            <div
-                class="card-img-overlay d-lg-flex d-lg-equal flex-wrap flex-column align-items-center justify-content-evenly">
-                <h1 class="display-4 my-3 fw-normal ">Find your dream job here</h1>
-                <p class="lead fw-normal text-center">And an even wittier subheading to boot. Jumpstart
-                    your
-                    marketing
-                    efforts with this example based on Apple's marketing pages.</p>
-                <a class="btn btn-secondary" href="#">Register Now</a>
+    <section class="border shadow">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner bg-dark">
+                <div class="carousel-item active">
+                    <img src="https://source.unsplash.com/1200x500/?deal" style="opacity: 30%" class="d-block w-100 bg-dark"
+                        alt="Heroes image">
+                </div>
+                <div class="carousel-caption d-none d-md-block my-lg-5">
+                    <h3>Find your dream job here</h3>
+                    <p>Some representative placeholder content for the first slide.And an even wittier subheading to boot.
+                        Jumpstart
+                        your
+                        marketing
+                        efforts with this example based on Apple's marketing pages.</p>
+                    <a class="btn btn-dark" href="/register">Register Now</a>
+                </div>
+            </div>
+            <div class="row justify-content-center px-3 py-4 d-block d-md-none">
+                <div class="col text-center ">
+                    <h3>Find your dream job here</h3>
+                    <p>Some representative placeholder content for the first slide.And an even wittier subheading to boot.
+                        Jumpstart
+                        your
+                        marketing
+                        efforts with this example based on Apple's marketing pages.</p>
+                    <a class="btn btn-dark" href="/register">Register Now</a>
+                </div>
             </div>
         </div>
     </section>
     <section>
-        <div class="row align-items-md-stretch">
-            <div class="col-md-6">
-                <div class="h-100 p-5 text-white bg-dark rounded-3">
-                    <h2>Looking for job?</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis excepturi dolorem nemo blanditiis
-                        ex cumque aperiam quam nesciunt libero assumenda?</p>
-                    <button class="btn btn-outline-light" type="button">Search job</button>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="h-100 p-5 bg-light border rounded-3">
-                    <h2>Want to hire people?</h2>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, neque odit asperiores est dicta at
-                        natus dolorem tempore rem quibusdam.</p>
-
-                    <button class="btn btn-outline-secondary" type="button">Post a job</button>
-                </div>
-            </div>
-        </div>
-
-    </section>
-    <section class="justify-content-center">
-        <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-            <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+        <div class="d-md-flex justify-content-evenly flex-md-equal w-100 my-md-3 ps-md-3">
+            <div class="bg-dark my-3 me-md-3 pt-3 px-3 shadow pt-md-5 px-md-5 text-center text-white overflow-hidden">
                 <div class="my-3 py-3">
-                    <h2 class="display-5">Find the great Job</h2>
-                    <p class="lead">And an even wittier subheading.</p>
+                    <h2 class="display-5">Job Seekers</h2>
+                    <p class="lead">Find and get better job opportunities according to skills and location.</p>
                 </div>
                 <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 200px; border-radius: 21px 21px 0 0;">
-                    <button class="btn btn-dark px-5 py-3" style="position:relative; top:40%;">Click</button>
+                    <a href="/jobs" class="btn btn-dark px-5 py-3" style="position:relative; top:40%;">Search a job</a>
                 </div>
             </div>
-            <div class="bg-light me-md-3 pt-3 px-3 border-1 pt-md-5 px-md-5 text-center overflow-hidden">
-                <div class="my-3 p-3">
-                    <h2 class="display-5">Hiring?</h2>
-                    <p class="lead">Best place jobs ads find best people.</p>
+            <div class="bg-light my-3  me-md-3 pt-3 px-3 shadow pt-md-5 px-md-5 text-center overflow-hidden">
+                <div class="my-3 py-3">
+                    <h2 class="display-5">Company</h2>
+                    <p class="lead">Post job advertisements to connect with the most potential job seekers.</p>
                 </div>
                 <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 200px; border-radius: 21px 21px 0 0;">
-                    <button class="btn btn-light px-5 py-3" style="position:relative; top:40%;">Click</button>
+                    <a href="/register/company" class="btn btn-light px-5 py-3" style="position:relative; top:40%;">Post a
+                        job</a>
                 </div>
             </div>
         </div>
