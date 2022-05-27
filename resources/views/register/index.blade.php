bcddb34c1c6e81a25bbc589d53302813df3a0622
DjorroDev <aufarmawarid@gmail.com>
2022-05-27 14:50:52 +0700

    Change the text page

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -6,17 +6,17 @@
     <div class="d-md-flex justify-content-around">
         <x-box class="bg-dark text-white">
             <h1 class="h4">Looking for a job</h1>
-            <p>Register as the job seeker Lorem ipsum dolor sit amet Lorem, ipsum dolor. lorem consectetur adipisicing elit.
-                Illo,
-                enim!</p>
-            <a href="/register/seeker" class="btn btn-light">Register</a>
+            <p>Show your abilities and qualities as a talent in an easy way. So that recruiters can see if you can be one of
+                the candidates for a job interview.</p>
+            <a href="/register/seeker" class="btn btn-light">Register as seeker</a>
         </x-box>
         <p class="align-self-center text-center lead">OR</p>
         <x-box>
             <h1 class="h3">Looking for a candidate</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel amet, debitis totam dolorum adipisci cupiditate.
+            <p>Recruiting candidates is indeed a challenge. We therefore understand that a job recruiter can require
+                multiple job recruitment platforms.
             </p>
-            <a href="/register/company" class="btn btn-dark">Register</a>
+            <a href="/register/company" class="btn btn-dark">Register as company</a>
         </x-box>
     </div>
     <div class="text-center">
